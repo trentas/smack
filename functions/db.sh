389b9cd1ffff2b9f397bef5f34c9386162894fa4
Fabio Wakim Trentini <trentas@gmail.com>
2016-04-29 08:04:58 -0300

    Wip db functions

--- functions/db.sh
+++ functions/db.sh
@@ -1,14 +1,27 @@
-# s.db.check? host user password
-# Checks if mysql client exists and can connect to the database
+# s.db.check.user? db_type host user password
+# Checks if db client exists and can connect to db server
 # fatal error
-function s.db.check?() {
+function s.db.check.user?() {
 
 }
 
-# s.db.check.database? host user password database
+# s.db.check.database? db_type host user password db_name
 # Checks if a database exists
 # returns error if it doesn't exists
 function s.db.check.database?() {
 
 }
 
+# s.db.create.user db_type host root_password user password
+# Creates a user with a password in db server
+function s.db.create.user() {
+
+}
+
+# s.db.create.database db_type host root_password db_name
+# Creates a new database 
+function s.db.create.database() {
+
+}
+
+# s.db.set.grants db_type host root_password user grant1...grantN
