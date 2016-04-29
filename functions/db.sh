d9339b456852164c675818009ee69b7f67c83573
Fabio Wakim Trentini <trentas@gmail.com>
2016-04-29 07:14:11 -0300

    Db

---
+++ functions/db.sh
@@ -0,0 +1,14 @@
+# s.db.check? host user password
+# Checks if mysql client exists and can connect to the database
+# fatal error
+function s.db.check?() {
+
+}
+
+# s.db.check.database? host user password database
+# Checks if a database exists
+# returns error if it doesn't exists
+function s.db.check.database?() {
+
+}
+
