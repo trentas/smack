c4b05d5546b97ade93eae4124b31a860da0ac0fd
Fabio Trentini <trentas@gmail.com>
2016-04-29 08:43:41 -0300

    WiP db

--- functions/db.sh
+++ functions/db.sh
@@ -1,27 +1,37 @@
-# s.db.check.user? db_type host user password
+# s.db.check.user? db_type db_host db_user db_password
 # Checks if db client exists and can connect to db server
 # fatal error
 function s.db.check.user?() {
 
 }
 
-# s.db.check.database? db_type host user password db_name
+# s.db.check.database? db_type db_host db_user db_password db_name
 # Checks if a database exists
 # returns error if it doesn't exists
 function s.db.check.database?() {
 
 }
 
-# s.db.create.user db_type host root_password user password
+# s.db.create.user db_type db_host db_root_password db_user db_password
 # Creates a user with a password in db server
 function s.db.create.user() {
 
 }
 
-# s.db.create.database db_type host root_password db_name
+# s.db.create.database db_type db_host db_root_password db_name
 # Creates a new database 
 function s.db.create.database() {
 
 }
 
-# s.db.set.grants db_type host root_password user grant1...grantN
+# s.db.set.grants db_type db_host db_root_password db_name db_user grant1...grantN
+# Set permissions to a specific user access the database
+function s.db.set.grants() {
+
+}
+
+# s.db.set.password db_type db_host db_root_password db_user db_password
+# Set new password to a specific user
+function s.db.set.password() {
+
+}
